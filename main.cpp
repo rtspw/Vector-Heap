52bafe3e4ef93a6ca2cd90afc5ac77168de371b4
Richard Tang <richard.cx.tang@gmail.com>
2018-05-07 22:38:04 -0700

    Simple driver for testing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include "node.h"
+#include "avlbst.h"
 #include "heap.h"
-
 using namespace std;
 
 int main() {
-    heap a;
-    a.insert("4", 1, 1, 1);
-    a.insert("3", 1, 1, 1);
-    a.insert("5", 1, 1, 1);
-    a.insert("2", 1, 1, 1);
+
+    heap bb;
+
+    bb.insert("apple",1,1,1);
+    bb.insert("abba",1,1,1);
+    bb.insert("appl",1,1,1);
+    bb.insert("appla",1,1,1);
+    bb.insert("apricot",1,1,1);
+    bb.insert("alice",1,1,1);
+    bb.insert("a",1,1,1);
+    bb.insert("an",1,1,1);
+    bb.insert("answer",1,1,1);
+
+    while(!bb.empty()) {
+        cout << bb.pop().word << endl;
+    }
     return 0;
+
 }
